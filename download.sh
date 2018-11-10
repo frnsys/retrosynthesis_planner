6e028d4a66f9ef6af95f0079ed7b0b6c276d8939
Francis Tseng <f+accounts@frnsys.com>
2018-11-09 22:51:48 -0500

    download script fix

--- download.sh
+++ download.sh
@@ -1,6 +1,9 @@
 cd data
 wget https://ndownloader.figshare.com/articles/5104873/versions/1 -O uspto_reactions.zip
 unzip uspto_reactions.zip
+7z x 1976_Sep2016_USPTOgrants_smiles.7z
+7z x 2001_Sep2016_USPTOapplications_smiles.7z
+rm *.{7z,zip}
 tail -n +2 1976_Sep2016_USPTOgrants_smiles.rsmi | cut -f1 >> reactions.rsmi
 tail -n +2 2001_Sep2016_USPTOapplications_smiles.rsmi | cut -f1 >> reactions.rsmi
 echo $(cat reactions.rsmi | wc -l) reactions
