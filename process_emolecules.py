075ce0854040e0a22e8c3c36eaa16d4e20cb4637
Francis Tseng <f+accounts@frnsys.com>
2018-11-11 23:44:28 -0500

    forgot emolecules file is combined into one

--- process_emolecules.py
+++ process_emolecules.py
@@ -16,11 +16,10 @@ def process(smi):
     return smi.split('.')
 
 smiles = set()
-for fn in ['data/emolecules_orderbb.smi', 'data/emolecules_plus.smi']:
-    with open(fn, 'r') as f:
-        for smis in tqdm(map(process, f)):
-            if smis is None: continue
-            for smi in smis:
-                smiles.add(smi)
+with open('data/emolecules.smi', 'r') as f:
+    for smis in tqdm(map(process, f)):
+        if smis is None: continue
+        for smi in smis:
+            smiles.add(smi)
 
 print('Compounds:', len(smiles))
