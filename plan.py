b2829c1addc964c02f31004156589aa0b2fdf920
Francis Tseng <f+accounts@frnsys.com>
2018-11-15 12:52:33 -0500

    re-wrote plan script for 3N-MCTS

---
+++ plan.py
@@ -0,0 +1,141 @@
+import json
+import molvs
+import random
+import policies
+from tqdm import tqdm
+from mcts import Node, mcts
+import tensorflow as tf
+from rdkit import Chem
+from rdkit.Chem import AllChem
+
+# Load base compounds
+starting_mols = set()
+with open('data/emolecules.smi', 'r') as f:
+    for line in tqdm(f, desc='Loading base compounds'):
+        smi = line.strip()
+        smi = molvs.standardize_smiles(smi)
+        starting_mols.add(smi)
+print('Base compounds:', len(starting_mols))
+
+# Load policy networks
+with open('model/rules.json', 'r') as f:
+    rules = json.load(f)
+    rollout_rules = rules['rollout']
+    expansion_rules = rules['expansion']
+
+rollout_net = policies.RolloutPolicyNet(n_rules=len(rollout_rules))
+expansion_net = policies.ExpansionPolicyNet(n_rules=len(expansion_rules))
+filter_net = policies.InScopeFilterNet()
+
+sess = tf.Session()
+init = tf.global_variables_initializer()
+sess.run(init)
+saver = tf.train.Saver()
+saver.restore(sess, 'model/model.ckpt')
+
+
+def transform(mol, rule):
+    # Apply rule
+    rxn = AllChem.ReactionFromSmarts(rule)
+    results = rxn.RunReactants([mol])
+
+    # Only look at first set of results (TODO any reason not to?)
+    results = results[0]
+    reactants = [Chem.MolToSmiles(smi) for smi in results]
+    return reactants
+
+def expansion(node):
+    """Try expanding each molecule in the current state
+    to possible reactants"""
+
+    # Assume each mol is a SMILES string
+    mols = node.state
+
+    # Convert mols to format for prediction
+    # If the mol is in the starting set, ignore
+    mols = [mol for mol in mols if mol not in starting_mols]
+    fprs = policies.fingerprint_mols(mols)
+
+    # Predict applicable rules
+    preds = sess.run(expansion_net.pred_op, feed_dict={
+        expansion_net.keep_prob: 1.,
+        expansion_net.X: fprs,
+        expansion_net.k: 5
+    })
+
+    # Generate children for reactants
+    children = []
+    for mol, rule_idxs in zip(mols, preds):
+        # State for children will
+        # not include this mol
+        new_state = mols - {mol}
+
+        mol = Chem.MolFromSmiles(mol)
+        for idx in rule_idxs:
+            # Extract actual rule
+            rule = expansion_rules[idx]
+
+            # TODO filter_net should check if the reaction will work?
+            # should do as a batch
+
+            # Apply rule
+            reactants = transform(mol, rule)
+
+            if not reactants: continue
+
+            state = new_state | set(reactants)
+            terminal = all(mol in starting_mols for mol in state)
+            child = Node(state=state, is_terminal=terminal, parent=node)
+            children.append(child)
+    return children
+
+
+def rollout(node, max_depth=200):
+    cur = node
+    for _ in range(max_depth):
+        if cur.is_terminal:
+            break
+
+        # Select a random mol (that's not a starting mol)
+        mols = [mol for mol in cur.state if mol not in starting_mols]
+        mol = random.choice(mols)
+        fprs = policies.fingerprint_mols([mol])
+
+        # Predict applicable rules
+        preds = sess.run(rollout_net.pred_op, feed_dict={
+            expansion_net.keep_prob: 1.,
+            expansion_net.X: fprs,
+            expansion_net.k: 1
+        })
+
+        rule = rollout_rules[preds[0][0]]
+        reactants = transform(Chem.MolFromSmiles(mol), rule)
+        state = cur.state | set(reactants)
+
+        # State for children will
+        # not include this mol
+        state = state - {mol}
+
+        terminal = all(mol in starting_mols for mol in state)
+        cur = Node(state=state, is_terminal=terminal, parent=cur)
+
+    # Max depth exceeded
+    else:
+        print('Rollout reached max depth')
+        return 0.
+
+    # TODO look up rewards from paper
+    return 1.
+
+
+# target_mol = '[H][C@@]12OC3=C(O)C=CC4=C3[C@@]11CCN(C)[C@]([H])(C4)[C@]1([H])C=C[C@@H]2O'
+target_mol = 'CC(=O)NC1=CC=C(O)C=C1'
+root = Node(state={target_mol})
+
+path = mcts(root, expansion, rollout, iterations=2000, max_depth=200)
+if path is None:
+    print('No synthesis path found. Try increasing `iterations` or `max_depth`.')
+else:
+    print('Path found:')
+    print(path)
+    import ipdb; ipdb.set_trace()
